be05661bdff717a2232b36c8f29963e1aa9c8a10
Lisitius <benjamin.fichaux@gmail.com>
2023-09-21 19:03:09 +0200

    setup tailwind, add react-touter and redux

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
+function Header() {
+  const location = useLocation();
+
+  return (
+    <div className="bg-customGreen p-4 flex justify-between">
+      <div className="text-lg font-semibold">Wealth Health</div>
+      <nav className="flex space-x-4">
+        <NavLink
+          to="/"
+          className={location.pathname === "/" ? "underline" : ""}
+          end
+        >
+          Employee List
+        </NavLink>
+        <NavLink
+          to="/add"
+          className={location.pathname === "/add" ? "underline" : ""}
+        >
+          Add Employee
+        </NavLink>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/AddEmployee.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddEmployee = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">HELLO WORLD ADD</h1>
+    </div>
+  );
+};
+
+export default AddEmployee;
