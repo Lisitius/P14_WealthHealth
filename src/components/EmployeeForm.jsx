d3db37e43c2adcc219f13bfcfde9539d26806166
Lisitius <benjamin.fichaux@gmail.com>
2023-09-27 17:12:49 +0200

    updating the form with handling of certain errors

--- src/components/EmployeeForm.jsx
+++ src/components/EmployeeForm.jsx
@@ -3,7 +3,13 @@ import { mockDepartment } from "../mockData/mockDepartment";
 import useEmployeeForm from "../hooks/useEmployeeForm";
 
 const EmployeeForm = () => {
-  const initialState = {
+  const {
+    employeeData,
+    handleInputChange,
+    handleSubmit,
+    formErrors,
+    isSuccess,
+  } = useEmployeeForm({
     firstName: "",
     lastName: "",
     dateOfBirth: "",
@@ -13,19 +19,22 @@ const EmployeeForm = () => {
     city: "",
     zipCode: "",
     state: "",
-  };
-
-  const { employeeData, handleInputChange, handleSave } =
-    useEmployeeForm(initialState);
+  });
 
   return (
     <div className="mt-2 w-1/2 mx-auto p-4 shadow-lg font-custom">
       <h2 className="text-2xl font-bold mb-5 text-center">Add Employee</h2>
+      {isSuccess && (
+        <div className="bg-green-900 text-white text-center p-2 mb-4 rounded">
+          Employee added successfully!
+        </div>
+      )}
       <div className="grid grid-cols-2 gap-4">
-        {/* Partie Identité Employé */}
+        {/* Employee Identity Part */}
         <div>
           <h3 className="text-lg font-semibold mb-3">Employee identity</h3>
           <div className="mb-3">
+            {/* firstname */}
             <label htmlFor="firstName" className="block mb-2">
               First Name
             </label>
@@ -35,10 +44,18 @@ const EmployeeForm = () => {
               id="firstName"
               value={employeeData.firstName}
               onChange={handleInputChange}
-              className="w-full p-2 border-solid border border-black rounded"
+              className={`w-full p-2 border-solid border border-black rounded ${
+                formErrors.firstName && "border-red-500"
+              }`}
             />
+            {formErrors.firstName && (
+              <p className="text-red-500 text-sm mt-1">
+                {formErrors.firstName}
+              </p>
+            )}
           </div>
           <div className="mb-3">
+            {/* lastname */}
             <label htmlFor="lastName" className="block mb-2">
               Last Name
             </label>
@@ -48,10 +65,16 @@ const EmployeeForm = () => {
               id="lastName"
               value={employeeData.lastName}
               onChange={handleInputChange}
-              className="w-full p-2 border-solid border border-black rounded"
+              className={`w-full p-2 border-solid border border-black rounded ${
+                formErrors.lastName ? "border-red-500" : ""
+              }`}
             />
+            {formErrors.lastName && (
+              <p className="text-red-500 text-sm mt-1">{formErrors.lastName}</p>
+            )}
           </div>
           <div className="mb-3">
+            {/* date of birth */}
             <label htmlFor="dateOfBirth" className="block mb-2">
               Date of Birth
             </label>
@@ -61,15 +84,23 @@ const EmployeeForm = () => {
               id="dateOfBirth"
               value={employeeData.dateOfBirth}
               onChange={handleInputChange}
-              className="w-full p-2 border-solid border border-black rounded"
+              className={`w-full p-2 border-solid border border-black rounded ${
+                formErrors.dateOfBirth ? "border-red-500" : ""
+              }`}
             />
+            {formErrors.dateOfBirth && (
+              <p className="text-red-500 text-sm mt-1">
+                {formErrors.dateOfBirth}
+              </p>
+            )}
           </div>
         </div>
 
-        {/* Partie Informations Employé */}
+        {/* Employee Information Part */}
         <div>
           <h3 className="text-lg font-semibold mb-3">Employee information</h3>
           <div className="mb-3">
+            {/* start date */}
             <label htmlFor="startDate" className="block mb-2">
               Start Date
             </label>
@@ -79,10 +110,18 @@ const EmployeeForm = () => {
               id="startDate"
               value={employeeData.startDate}
               onChange={handleInputChange}
-              className="w-full p-2 border-solid border border-black rounded"
+              className={`w-full p-2 border-solid border border-black rounded ${
+                formErrors.startDate ? "border-red-500" : ""
+              }`}
             />
+            {formErrors.startDate && (
+              <p className="text-red-500 text-sm mt-1">
+                {formErrors.startDate}
+              </p>
+            )}
           </div>
           <div className="mb-3">
+            {/* department */}
             <label htmlFor="department" className="block mb-2">
               Department
             </label>
@@ -91,7 +130,9 @@ const EmployeeForm = () => {
               id="department"
               value={employeeData.department}
               onChange={handleInputChange}
-              className="w-full p-2 border-solid border border-black rounded"
+              className={`w-full p-2 border-solid border border-black rounded ${
+                formErrors.department ? "border-red-500" : ""
+              }`}
             >
               <option value="">Select Department</option>
               {mockDepartment.map((dept, index) => (
@@ -100,14 +141,20 @@ const EmployeeForm = () => {
                 </option>
               ))}
             </select>
+            {formErrors.department && (
+              <p className="text-red-500 text-sm mt-1">
+                {formErrors.department}
+              </p>
+            )}
           </div>
         </div>
       </div>
 
-      {/* Partie Adresse Employé */}
+      {/* Party Address Employee */}
       <div className="mb-3">
         <h3 className="text-lg font-semibold mb-3">Employee address</h3>
         <div className="mb-3">
+          {/* street */}
           <label htmlFor="street" className="block mb-2">
             Street
           </label>
@@ -117,10 +164,16 @@ const EmployeeForm = () => {
             id="street"
             value={employeeData.street}
             onChange={handleInputChange}
-            className="w-full p-2 border-solid border border-black rounded"
+            className={`w-full p-2 border-solid border border-black rounded ${
+              formErrors.street ? "border-red-500" : ""
+            }`}
           />
+          {formErrors.street && (
+            <p className="text-red-500 text-sm mt-1">{formErrors.street}</p>
+          )}
         </div>
         <div className="mb-3">
+          {/* city */}
           <label htmlFor="city" className="block mb-2">
             City
           </label>
@@ -130,10 +183,16 @@ const EmployeeForm = () => {
             id="city"
             value={employeeData.city}
             onChange={handleInputChange}
-            className="w-full p-2 border-solid border border-black rounded"
+            className={`w-full p-2 border-solid border border-black rounded ${
+              formErrors.city ? "border-red-500" : ""
+            }`}
           />
+          {formErrors.city && (
+            <p className="text-red-500 text-sm mt-1">{formErrors.city}</p>
+          )}
         </div>
         <div className="mb-3">
+          {/* zip code */}
           <label htmlFor="zipCode" className="block mb-2">
             Zip Code
           </label>
@@ -143,10 +202,16 @@ const EmployeeForm = () => {
             id="zipCode"
             value={employeeData.zipCode}
             onChange={handleInputChange}
-            className="w-full p-2 border-solid border border-black rounded"
+            className={`w-full p-2 border-solid border border-black rounded ${
+              formErrors.zipCode ? "border-red-500" : ""
+            }`}
           />
+          {formErrors.zipCode && (
+            <p className="text-red-500 text-sm mt-1">{formErrors.zipCode}</p>
+          )}
         </div>
         <div className="mb-3">
+          {/* state */}
           <label htmlFor="state" className="block mb-2">
             State
           </label>
@@ -155,7 +220,9 @@ const EmployeeForm = () => {
             id="state"
             value={employeeData.state}
             onChange={handleInputChange}
-            className="w-full p-2 border-solid border border-black rounded"
+            className={`w-full p-2 border-solid border border-black rounded ${
+              formErrors.state ? "border-red-500" : ""
+            }`}
           >
             <option value="">Select state</option>
             {mockStates.map((state, index) => (
@@ -164,13 +231,16 @@ const EmployeeForm = () => {
               </option>
             ))}
           </select>
+          {formErrors.state && (
+            <p className="text-red-500 text-sm mt-1">{formErrors.state}</p>
+          )}
         </div>
       </div>
 
       <button
         type="button"
-        onClick={handleSave}
         className="bg-customGreen w-full mt-3 p-2 text-white font-semibold rounded"
+        onClick={handleSubmit}
       >
         Save Employee
       </button>
